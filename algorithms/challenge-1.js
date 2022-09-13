a0f4d21acc271ca996cd9d91771705a39b6bfadc
Orbemt <113280310+Orbemt@users.noreply.github.com>
2022-09-13 15:52:05 +0800

    Update challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,10 @@
-function countTruthy(arr) {
-
+function countTruthy(arr) {let count = 0;
+    for (let item of arr) {
+        if (item) {
+          count++;
+        }
+    }
+    console.log(count)
+    return count;
 }
 module.exports = countTruthy
