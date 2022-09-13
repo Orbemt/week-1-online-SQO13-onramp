584f33fd60e1bbd5eb28dfb005eb63e7a6af6032
Orbemt <113280310+Orbemt@users.noreply.github.com>
2022-09-13 16:35:05 +0800

    Update challenge-2.js

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,5 +1,16 @@
 function fizzBuzz(input){
-
+if (input % 3 === 0 && input % 5 === 0) {
+    console.log("Fizz Buzz")
+    return "Fizz Buzz";
+  } else if (input % 3 === 0) {
+    console.log("Fizz")
+    return "Fizz";
+  } else if (input % 5 === 0) {
+    console.log("Buzz")
+    return "Buzz";
+  } 
+  console.log(input)
+  return input; 
   
 }
 
